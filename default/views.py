2481821860d0c4cec41b6fdbd2ab7ff37ec6fe59
unknown <wencheng0109@gmail.com>
2019-10-31 11:45:22 +0800

    新增使用基底

--- default/views.py
+++ default/views.py
@@ -12,3 +12,8 @@ class PollList(ListView):
 
 class PollDetel(DetailView):
     model = poll
+
+    def get_context_data(self, **kwargs):
+        ctx = super ().get_context_data(**kwargs)
+        ctx['option']=Option.object.filter(poll_idself.kwargs['pk'])
+        return ctx
